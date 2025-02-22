1264e11beae51343476fca15212d5869da2802d3
congtran19 <tranthanhcong198hy@gmail.com>
2025-02-22 16:17:05 +0700

    Lenh Nhap Xuat Du Lieu

--- src/Java1/HelloWorld.java
+++ src/Java1/HelloWorld.java
@@ -1,8 +1,29 @@
 package Java1;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int a = 6;
+		int b = 9;
+		int c = a + b;
+		System.out.println("Sum" + c);
+		System.out.print("Hoi dan it");
+		System.out.print("\nHoi dan it");
+
+		System.out.printf("Co %.3f nguoi dang ki ", 4000.55);
+
+		System.out.printf("Co %.0f nguoi dang ki ", 4000.55);
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
+		System.out.println("My name is " + name + ", age = " + age);
+		scanner.close();
+
 	}
 
 }
