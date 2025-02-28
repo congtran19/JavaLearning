bc1a014795d32ce12f5d574840004a9cea2222a7
congtran19 <tranthanhcong198hy@gmail.com>
2025-02-28 14:07:40 +0700

    Loop

---
+++ src/Java1/Loop.java
@@ -0,0 +1,15 @@
+package Java1;
+
+public class Loop {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.print(i);
+		}
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i < 5);
+	}
+
+}
